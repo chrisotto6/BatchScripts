3b2f02aee7ed59e3cb17cbc1ceb75384dfb02f02
Chris <chris.otto6@gmail.com>
2015-01-21 10:54:45 -0600

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-choco install chocolatey
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco install chocolateygui
 
