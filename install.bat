3154bb9dd002b239e4e3eae00bc7253c33155c41
unknown <1019491@1019491WIN7LT.jda.corp.local>
2015-01-21 10:39:13 -0600

    first commit

---
+++ install.bat
@@ -0,0 +1,44 @@
+choco install chocolatey
+
+choco install chocolateygui
+
+choco install tcc
+
+choco install git
+
+choco install notepadplusplus.install
+
+choco install google-chrome-x64
+
+choco install fiddler
+
+choco install procexp
+
+choco install sublimetext2
+
+choco install ant
+
+choco install nodejs.install
+
+choco install ruby
+
+choco install python
+
+choco install filezilla
+
+choco install googledrive
+
+choco install virtualbox
+
+choco install utorrent
+
+choco install speccy
+
+choco install visualstudio2013ultimate
+choco install visualstudio2013professional
+
+choco install android-sdk
+
+choco install eclipse
+
+choco install netbeans
